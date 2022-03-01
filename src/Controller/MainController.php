0f4e116f9831c8ae2181e9967a8d417c2e685b14
dautricourtl <55079792+dautricourtl@users.noreply.github.com>
2022-03-01 11:54:32 +0100

    mise en place login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
@@ -19,7 +20,10 @@ class MainController extends AbstractController
     #[Route('/login', name: 'login')]
     public function login(): Response
     {
-        return $this->render('main/login.html.twig');
+        return $this->render('main/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
     }
 
     #[Route('/profile', name: 'profile')]
