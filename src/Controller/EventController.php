66869a5314235e62f146c5e7f5c0028e0ea44df5
Administrateur <claire.brogard2020@campus-eni.fr>
2022-03-04 09:45:47 +0100

    condition event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,8 +8,9 @@ use App\Entity\Event;
 use App\Entity\State;
 use App\Form\EventType;
 use App\Entity\Location;
-use App\Repository\CityRepository;
+use App\Form\ProfileType;
 use PhpParser\Builder\Method;
+use App\Repository\CityRepository;
 use App\Repository\SiteRepository;
 use App\Repository\UserRepository;
 use App\Repository\EventRepository;
@@ -25,69 +26,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class EventController extends AbstractController
 {
 
-  #[Route('/ajoutLocation', name:'add_location')]
-  public function ajouterLocation(EntityManagerInterface $em, LocationRepository $LocationRepository, CityRepository $cityRepository) {
-    $location = new Location();
-
-    $location -> setName('Un bar');
-    $location -> setAdress("l'adresse du bar");
-    $location -> setLatitude("la latitude du bar");
-    $location -> setLongitude("la longitude du bar");
-    $location -> setCity(new City("Nantes", "44000"));
-
-    $em ->persist($location);
-    $em ->flush();
-
-    return new Response("Jeu d'essai inséré");
-  }
-
-  #[Route('/ajoutState', name:'add_state')]
-  public function ajouterState(EntityManagerInterface $em) {
-    $state = new State();
-
-    $state -> setName("etat");
-    $em ->persist($state);
-    $em ->flush();
-   return new Response("Jeu d'essai inséré");
-  }
-
-  #[Route('/ajoutSite', name:'add_site')]
-  public function ajouterStite(EntityManagerInterface $em) {
-    $site = new Site();
-
-    $site -> setName("site");
-    $em ->persist($site);
-    $em ->flush();
-   return new Response("Jeu d'essai inséré");
-  }
-
-  #[Route('/ajoutCity', name:'add_city')]
-  public function ajouterCity(EntityManagerInterface $em) {
-    $city = new City();
-    $city -> setName("Nantes");
-    $city -> setZipCode("44000");
-    $em ->persist($city);
-    $em ->flush();
-   return new Response("Jeu d'essai inséré");
-  }
-
-  #[Route('/ajoutOwner', name:'add_owner')]
-  public function ajouterOwner(EntityManagerInterface $em, SiteRepository $siteRepository) {
-    $owner = new User();
-    $owner -> setPseudo("bloup");
-    $owner -> setPassword("bloup");
-    $owner -> setName("bloup");
-    $owner -> setSurname("bloup");
-    $owner -> setTel("bloup");
-    $owner -> setMail("bloup");   
-    $owner -> isActive(true);  
-    $site = $siteRepository->findById(1)[0];
-    $owner ->setSite($site);
-    $em ->persist($owner);
-    $em ->flush();
-   return new Response("Jeu d'essai inséré");
-  }
-
+  
   #[Route('/addEvent', name: 'app_event')]
   public function addEvent(Request $request, EntityManagerInterface $em, StateRepository $stateRepository, UserRepository $userRepository, CityRepository $cityRepository) : Response {
       $event = new Event();
@@ -162,6 +101,58 @@ class EventController extends AbstractController
     }
 
     }
+
+
+  #[Route('/updateEvent/{id}', name: 'event_update', requirements: ['id'=> '\d+'], methods:['GET'])]
+    public function update(Request $request, $id, EntityManagerInterface $em, EventRepository $eventRepository): Response {
+        
+        $event = $eventRepository ->findOneById($id);
+        $formbuilder = $this->createForm(EventType::class, $event);
+        $formbuilder->handleRequest($request);
+
+        if($formbuilder->isSubmitted() && $formbuilder->isValid()){
+          $dateDebutVerification = $event->getBeginAt();
+          $dateLimiteVerification = $event->getLimitInscriptionAt();
+          $durationVerification = $event->getDuration();
+          $nbinscriptionVerifcation = $event->getInscriptionMax();
+          
+          if($durationVerification < 0 ||$nbinscriptionVerifcation < 0||$dateLimiteVerification >= $dateDebutVerification ){
+  
+            if($durationVerification < 0) {
+              $this ->addFlash('danger', 'La durée doit être positive');
+            } 
+            if($nbinscriptionVerifcation < 0) {
+                $this ->addFlash('danger', 'Le nombre d\'inscrits doit être positif');
+            }
+            if($dateLimiteVerification >= $dateDebutVerification) {
+              $this ->addFlash('danger', 'La date limite d\'inscription doit être antérieure à la date de début');
+            }
+  
+            $eventForm = $formbuilder->createView();
+            return $this->render('main/event.html.twig', ['eventForm' => $eventForm]);
+        }
+          $uploadedFile = $formbuilder['imageFile']->getData();  
+          if($uploadedFile){
+          $destination = $this->getParameter('kernel.project_dir').'/public/uploads/images/event';  
+              $newFilename = uniqid().'.'.$uploadedFile->guessExtension();
+              $uploadedFile->move(
+                  $destination,
+                  $newFilename
+              );
+          $event->setPhoto($newFilename);
+        }
+          $em->persist($event);
+          $em->flush();  
+          $this ->addFlash('success', 'La sortie a bien été éditée');
+          return $this->redirectToRoute('event_detail');
+        }
+      
+   
+      $eventForm = $formbuilder->createView();
+      return $this->render('main/event.html.twig', ['eventForm' => $eventForm]);
+    }
+    
+
   }
 
 
