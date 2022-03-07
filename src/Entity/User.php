602b733b078b914eb6e8881cf267dfd774e16279
CorentinLebris <88657732+CorentinLebris@users.noreply.github.com>
2022-03-07 14:32:39 +0100

    creation db question

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -65,6 +65,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $photo;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $reponse;
+
+    #[ORM\ManyToOne(targetEntity: Question::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $question;
+
     public function __construct()
     {
         $this->events = new ArrayCollection();
@@ -383,6 +390,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPhoto(?string $photo): self
     {
         $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getReponse(): ?string
+    {
+        return $this->reponse;
+    }
+
+    public function setReponse(string $reponse): self
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?question $question): self
+    {
+        $this->question = $question;
 
         return $this;
     }
